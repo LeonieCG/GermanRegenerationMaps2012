12931ac524582f9bb4135f093dea112997ae0419
LeonieCG <leonie.gass@uni-bayreuth.de>
2025-05-29 08:03:56 +0000

    added note and predictor selection

--- script/Predict_basalarea.R
+++ script/Predict_basalarea.R
@@ -1,3 +1,5 @@
+# This script predicts species-specific basal area for Germany
+
 start.script <- Sys.time()
 
 packages <- c("tidyr", "dplyr","magrittr", "terra", "mgcv", "gstat", "DHARMa",
@@ -14,6 +16,23 @@ source("script/Prediction_function.R")
 
 # Choose variables ----------------------------------------------------------
 source("script/Model_vars.R")
+
+# due to licencing, not all environmental predictors could be published with this study 
+# the following is a selection, model outcomes can thereby vary highly from the study!
+resp = "count"
+fixed = c("tPeriodic2020_forcli", "tSeas2020_forcli","tMinColdMonth2020_forcli","tRangeDay2020_forcli","tRangeAn2020_forcli" #T microclimate
+          , "tPeriodic2010_chelsa", "tSeas2010_chelsa","tMinColdMonth2010_chelsa","tRangeDay2010_chelsa","tRangeAn2010_chelsa" #T macroclimate
+          , "precPeriodic2010_chelsa", "precSeas2010_chelsa" # prec
+          , "wwpi_cop" # Water prob index, Anoxy indicator, water bodies and flooded plains
+          , "tcd_cop" #tree cover density
+          , "alt", "northexp", "eastexp" # terrain vars
+          , "wzp12_ba_ha_species" #Basal area of respective old trees
+)
+random = c("yearmonth" # to account for changes within sampling period
+           , "blname" # Bundesland
+)
+spatial = c("x","y")
+
 resp = "wzp12_ba_ha_species" 
 fixed = fixed[! fixed %in% c("wzp12_ba_ha_species")]# remove wzp12 because it is not needed
 
