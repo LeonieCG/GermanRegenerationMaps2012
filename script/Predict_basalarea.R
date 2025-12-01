001224bff6e3a59dedfec9db5cde7456fc870fec
LeonieCG <leonie.gass@uni-bayreuth.de>
2025-12-01 10:05:34 +0000

    changed naming of files, fixed nesting bug, moved script run timing

--- script/Predict_basalarea.R
+++ script/Predict_basalarea.R
@@ -27,28 +27,27 @@ stacked <- c(rast("data/Predictor_100m_Germany/x.tif"),
 for(species in species.vect) {
   print(species)
   try({
-    prediction = predict.fit(fit = readRDS(paste0("output/Fits/Basalarea/wzp12_interpol/",species,"/Basalarea_",species, "_fit.rds")),
+    prediction = predict.fit(fit = readRDS(paste0("output/Fits/Basalarea/wzp12/",species,"/Basalarea_",species, "_fit.rds")),
                              predictorstack = stacked,
-                             exclude = NULL ,#"yearmonth",
+                             exclude = NULL ,
                              BAspecies = NULL,
                              get_dir = NULL,
                              haconvert = FALSE, # converts to hectare
                              CI = FALSE, # Only use when link function is log
                              cores = 20) # for parallel
-    writeRaster(prediction, paste0("data/Predictor_100m_Germany/wzp12_ba_ha_species_interpol_",species,".tif"), overwrite=TRUE)
+    writeRaster(prediction, paste0("data/Predictor_100m_Germany/wzp12_ba_ha_species_",species,".tif"), overwrite=TRUE)
   }, FALSE)
  }
 
-
-print(paste("Script took",Sys.time()-start.script, units(Sys.time()-start.script)))
 
 
 # Retrieve predicted basal area values at plot location ------------------------------------
 coord <- readRDS("data/DE_BWI3_explvars_sv.rds") %>% 
   select(plotid)
 
+
 for(species in species.vect){
-  ba  <- rast(paste0("data/Predictor_100m_Germany/wzp12_ba_ha_species_interpol_",species,".tif"))
+  ba  <- rast(paste0("data/Predictor_100m_Germany/wzp12_ba_ha_species_",species,".tif"))
   vals <- extract(ba, coord, ID=FALSE)  # extract basal area
   coord <- cbind(coord, vals) # combine
 }
@@ -57,6 +56,8 @@ wzp12_interpol <- coord %>%
   as.data.frame() %>% 
   pivot_longer(cols = 2:dim(.)[2], names_to = "tax", values_to = "wzp12_ba_ha_species") %>% 
   group_by(tax) %>% 
-  nest
-  
+  nest()
+
 saveRDS(wzp12_interpol, "data/DE_BWI3_big_basalarea_wzp12_interpol.rds")
+
+print(paste("Script took",Sys.time()-start.script, units(Sys.time()-start.script)))
