e3a314c8d7b20753f641592ddb0c3dfa7b604174
LeonieCG <leonie.gass@uni-bayreuth.de>
2025-12-09 09:25:15 +0000

    did not change anything

--- script/Model_saplings.R
+++ script/Model_saplings.R
@@ -76,7 +76,7 @@ for(species in species.vect) {
                   s.bs = "cs",
                   spat.which = "tensor",
                   spat.bs = "cs", # is faster than ts
-                  spat.k= "c(25,50)", 
+                  spat.k= "c(25,50)",
                   select.var = FALSE,
                   bam = TRUE,
                   Data = mv,
